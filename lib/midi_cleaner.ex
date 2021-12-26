8f22b33de3589fd65aad3ccb4f6bf2e0624d6dd0
Pat Maddox <pat@patmaddox.com>
2021-12-26 12:41:25 -0800

    Set MIDI channel to 0

--- lib/midi_cleaner.ex
+++ lib/midi_cleaner.ex
@@ -5,6 +5,10 @@ defmodule MidiCleaner do
     %{sequence | tracks: remove_track_program_changes(sequence.tracks)}
   end
 
+  def set_midi_channel(%Sequence{} = sequence) do
+    %{sequence | tracks: set_track_midi_channel(sequence.tracks)}
+  end
+
   defp remove_track_program_changes(tracks) when is_list(tracks),
     do: Enum.map(tracks, &remove_track_program_changes/1)
 
@@ -16,4 +20,25 @@ defmodule MidiCleaner do
 
   defp event_is_program_change?(%Event{symbol: :program}), do: true
   defp event_is_program_change?(%Event{}), do: false
+
+  defp set_track_midi_channel(tracks) when is_list(tracks),
+    do: Enum.map(tracks, &set_track_midi_channel/1)
+
+  defp set_track_midi_channel(%Track{} = track) do
+    %{track | events: set_event_midi_channel(track.events)}
+  end
+
+  defp set_event_midi_channel(events) when is_list(events),
+    do: Enum.map(events, &set_event_midi_channel/1)
+
+  defp set_event_midi_channel(%Event{} = event) do
+    if Event.channel?(event) do
+      [first | rest] = event.bytes
+      channel = Event.channel(event)
+      first = first - channel
+      struct(event, bytes: [first | rest])
+    else
+      event
+    end
+  end
 end
--- test/midi_cleaner_test.exs
+++ test/midi_cleaner_test.exs
@@ -12,6 +12,7 @@ defmodule MidiCleanerTest do
     track_without_pc = %Track{events: [note_on]}
 
     sequence_with_pc = sequence(tracks: [track_with_pc, track_with_pc])
+
     sequence_without_pc = %Sequence{
       division: 480,
       conductor_track: conductor_track(),
@@ -19,6 +20,39 @@ defmodule MidiCleanerTest do
     }
 
     assert MidiCleaner.remove_program_changes(sequence_with_pc) == sequence_without_pc
+  end
+
+  test "set_midi_channel(sequence)" do
+    text_event = %Event{bytes: "Violoncello", delta_time: 0, symbol: :seq_name}
+    channel_0_controller = %Event{bytes: [176, 20, 0], delta_time: 0, symbol: :controller}
+    channel_15_controller = %Event{bytes: [191, 20, 0], delta_time: 0, symbol: :controller}
+    channel_0_note = %Event{bytes: [144, 74, 27], delta_time: 0, symbol: :on}
+    channel_15_note = %Event{bytes: [159, 74, 27], delta_time: 0, symbol: :on}
+
+    multi_channel_track = %Track{
+      events: [
+        text_event,
+        channel_0_controller,
+        channel_15_controller,
+        channel_0_note,
+        channel_15_note
+      ]
+    }
+
+    single_channel_track = %Track{
+      events: [
+        text_event,
+        channel_0_controller,
+        channel_0_controller,
+        channel_0_note,
+        channel_0_note
+      ]
+    }
+
+    multi_channel_sequence = sequence(tracks: [multi_channel_track, multi_channel_track])
+    single_channel_sequence = sequence(tracks: [single_channel_track, single_channel_track])
+
+    assert MidiCleaner.set_midi_channel(multi_channel_sequence) == single_channel_sequence
   end
 
   defp conductor_track() do
