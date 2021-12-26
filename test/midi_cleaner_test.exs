0871c552a0bfc54b0f90a92907e1101b27123abd
Pat Maddox <pat@patmaddox.com>
2021-12-26 12:45:23 -0800

    Refactor test

--- test/midi_cleaner_test.exs
+++ test/midi_cleaner_test.exs
@@ -5,8 +5,8 @@ defmodule MidiCleanerTest do
   alias Midifile.{Event, Sequence, Track}
 
   test "remove_program_changes(sequence)" do
-    program_change = %Event{bytes: [192, 40], delta_time: 0, symbol: :program}
-    note_on = %Event{bytes: [144, 72, 34], delta_time: 0, symbol: :on}
+    program_change = event(bytes: [192, 40], symbol: :program)
+    note_on = event(bytes: [144, 72, 34], symbol: :on)
 
     track_with_pc = %Track{events: [program_change, note_on]}
     track_without_pc = %Track{events: [note_on]}
@@ -23,11 +23,11 @@ defmodule MidiCleanerTest do
   end
 
   test "set_midi_channel(sequence)" do
-    text_event = %Event{bytes: "Violoncello", delta_time: 0, symbol: :seq_name}
-    channel_0_controller = %Event{bytes: [176, 20, 0], delta_time: 0, symbol: :controller}
-    channel_15_controller = %Event{bytes: [191, 20, 0], delta_time: 0, symbol: :controller}
-    channel_0_note = %Event{bytes: [144, 74, 27], delta_time: 0, symbol: :on}
-    channel_15_note = %Event{bytes: [159, 74, 27], delta_time: 0, symbol: :on}
+    text_event = event(bytes: "Violoncello", symbol: :seq_name)
+    channel_0_controller = event(bytes: [176, 20, 0], symbol: :controller)
+    channel_15_controller = event(bytes: [191, 20, 0], symbol: :controller)
+    channel_0_note = event(bytes: [144, 74, 27], symbol: :on)
+    channel_15_note = event(bytes: [159, 74, 27], symbol: :on)
 
     multi_channel_track = %Track{
       events: [
@@ -58,8 +58,8 @@ defmodule MidiCleanerTest do
   defp conductor_track() do
     %Track{
       events: [
-        %Event{symbol: :seq_name, bytes: "Unnamed"},
-        %Event{symbol: :tempo, bytes: [trunc(60_000_000 / 82)]}
+        event(symbol: :seq_name, bytes: "Unnamed"),
+        event(symbol: :tempo, bytes: [trunc(60_000_000 / 82)])
       ]
     }
   end
@@ -71,6 +71,13 @@ defmodule MidiCleanerTest do
         conductor_track: conductor_track(),
         tracks: []
       },
+      opts
+    )
+  end
+
+  defp event(opts) do
+    struct(
+      %Event{delta_time: 0},
       opts
     )
   end
