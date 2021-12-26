c4c7bc7e7e3c1c5d1010c9cf5aa39093cdc46cfb
Pat Maddox <pat@patmaddox.com>
2021-12-26 01:05:25 -0800

    New elixir app

---
+++ lib/midi_cleaner.ex
@@ -0,0 +1,18 @@
+defmodule MidiCleaner do
+  @moduledoc """
+  Documentation for `MidiCleaner`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MidiCleaner.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/midi_cleaner_test.exs
@@ -0,0 +1,8 @@
+defmodule MidiCleanerTest do
+  use ExUnit.Case
+  doctest MidiCleaner
+
+  test "greets the world" do
+    assert MidiCleaner.hello() == :world
+  end
+end
