cc5401b0f0c421f94bcfa0651b4f3625f6a028f1
Pat Maddox <pat@patmaddox.com>
2021-12-26 12:13:20 -0800

    Refactor test

--- test/midi_cleaner_test.exs
+++ test/midi_cleaner_test.exs
@@ -7,28 +7,37 @@ defmodule MidiCleanerTest do
   test "remove_program_changes(sequence)" do
     program_change = %Event{bytes: [192, 40], delta_time: 0, symbol: :program}
     note_on = %Event{bytes: [144, 72, 34], delta_time: 0, symbol: :on}
+
     track_with_pc = %Track{events: [program_change, note_on]}
     track_without_pc = %Track{events: [note_on]}
 
-    conductor = %Track{
+    sequence_with_pc = sequence(tracks: [track_with_pc, track_with_pc])
+    sequence_without_pc = %Sequence{
+      division: 480,
+      conductor_track: conductor_track(),
+      tracks: [track_without_pc, track_without_pc]
+    }
+
+    assert MidiCleaner.remove_program_changes(sequence_with_pc) == sequence_without_pc
+  end
+
+  defp conductor_track() do
+    %Track{
       events: [
         %Event{symbol: :seq_name, bytes: "Unnamed"},
         %Event{symbol: :tempo, bytes: [trunc(60_000_000 / 82)]}
       ]
     }
+  end
 
-    sequence = %Sequence{
-      division: 480,
-      conductor_track: conductor,
-      tracks: [track_with_pc, track_with_pc]
-    }
-
-    sequence_without_pc = %Sequence{
-      division: 480,
-      conductor_track: conductor,
-      tracks: [track_without_pc, track_without_pc]
-    }
-
-    assert MidiCleaner.remove_program_changes(sequence) == sequence_without_pc
+  defp sequence(opts) do
+    struct(
+      %Sequence{
+        division: 480,
+        conductor_track: conductor_track(),
+        tracks: []
+      },
+      opts
+    )
   end
 end
