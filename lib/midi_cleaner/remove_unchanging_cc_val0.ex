23dd6ede40402c0a9a1f76d77b55b3127571902a
Pat Maddox <pat@patmaddox.com>
2021-12-26 13:49:48 -0800

    Refactor: Keep function at same level of abstraction

--- lib/midi_cleaner/remove_unchanging_cc_val0.ex
+++ lib/midi_cleaner/remove_unchanging_cc_val0.ex
@@ -10,7 +10,7 @@ defmodule MidiCleaner.RemoveUnchangingCcVal0 do
 
   defp remove_track_unchanging_cc_val0(%Track{} = track) do
     controls_to_keep = controls_to_keep(track.events)
-    events = Enum.filter(track.events, &keep_event?(&1, controls_to_keep))
+    events = events_to_keep(track.events, controls_to_keep)
     %{track | events: events}
   end
 
@@ -23,6 +23,9 @@ defmodule MidiCleaner.RemoveUnchangingCcVal0 do
 
   defp keep_control?(%Event{bytes: [_, _, val]}) when val > 0, do: true
   defp keep_control?(_), do: false
+
+  defp events_to_keep(events, controls_to_keep),
+    do: Enum.filter(events, &keep_event?(&1, controls_to_keep))
 
   defp keep_event?(%Event{bytes: [cc, _, _], symbol: :controller}, controls_to_keep),
     do: MapSet.member?(controls_to_keep, cc)
