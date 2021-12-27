db7f8cb428f6b56b7d52cffcc03fc9fbe8a55fed
Pat Maddox <pat@patmaddox.com>
2021-12-26 23:45:37 -0800

    Implement a generic command runner

--- lib/midi_cleaner/runner.ex
+++ lib/midi_cleaner/runner.ex
@@ -1,6 +1,15 @@
 defmodule MidiCleaner.Runner do
   @callback run(List.t()) :: :ok
 
-  def run(_) do
-  end
+  def run([]), do: []
+  def run([command]), do: run_command(command)
+  def run([command | rest]), do: run_command(command) |> run(rest)
+
+  defp run(val, [command]), do: run_command(command, [val])
+  defp run(val, [command | rest]), do: run_command(command, [val]) |> run(rest)
+
+  defp run_command({command, args}, preargs), do: run_command(command, preargs ++ args)
+  defp run_command(command, args), do: apply(command, args)
+  defp run_command({command, args}), do: run_command(command, args)
+  defp run_command(command), do: apply(command, [])
 end
---
+++ test/runner_test.exs
@@ -0,0 +1,62 @@
+defmodule MidiCleaner.RunnerTest do
+  use ExUnit.Case
+  doctest MidiCleaner.Runner
+
+  alias MidiCleaner.Runner
+
+  describe "run()" do
+    test "no args" do
+      assert Runner.run([]) == []
+    end
+
+    test "one command" do
+      commands = [&__MODULE__.foo/0]
+      assert Runner.run(commands) == "foo"
+    end
+
+    test "two commands" do
+      commands = [
+        &__MODULE__.foo/0,
+        &String.upcase/1
+      ]
+
+      assert Runner.run(commands) == "FOO"
+    end
+
+    test "three commands" do
+      commands = [
+        &__MODULE__.foo/0,
+        &String.upcase/1,
+        &String.reverse/1
+      ]
+
+      assert Runner.run(commands) == "OOF"
+    end
+
+    test "one command with arg" do
+      commands = [{&String.upcase/1, ["foo"]}]
+      assert Runner.run(commands) == "FOO"
+    end
+
+    test "two commands with args" do
+      commands = [
+        {&String.upcase/1, ["foo"]},
+        {&String.replace/3, ["OO", "BAR"]}
+      ]
+
+      assert Runner.run(commands) == "FBAR"
+    end
+
+    test "three commands with args" do
+      commands = [
+        {&String.upcase/1, ["foo"]},
+        {&String.replace/3, ["OO", "BAR"]},
+        {&String.slice/2, [0..1]}
+      ]
+
+      assert Runner.run(commands) == "FB"
+    end
+  end
+
+  def foo, do: "foo"
+end
