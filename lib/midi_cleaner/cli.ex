898c9673406f55f7c5d1dfe481b7c2d9ee8cb76d
Pat Maddox <pat@patmaddox.com>
2021-12-27 00:08:14 -0800

    Remove one-element tuples

--- lib/midi_cleaner/cli.ex
+++ lib/midi_cleaner/cli.ex
@@ -8,25 +8,23 @@ defmodule MidiCleaner.CLI do
 
   defp parse_options(args) do
     {options, _} = OptionParser.parse!(args, strict: [pc: :boolean, cc0: :boolean, ch: :integer])
-    {Map.new(options)}
+    Map.new(options)
   end
 
-  defp setup_commands({opts}) when opts == %{} do
-    []
-  end
+  defp setup_commands(opts) when opts == %{}, do: []
 
-  defp setup_commands({%{pc: true} = opts}),
+  defp setup_commands(%{pc: true} = opts),
     do: append_command(opts, :pc, &MidiCleaner.remove_program_changes/1)
 
-  defp setup_commands({%{cc0: true} = opts}),
+  defp setup_commands(%{cc0: true} = opts),
     do: append_command(opts, :cc0, &MidiCleaner.remove_unchanging_cc_val0/1)
 
-  defp setup_commands({%{ch: channel} = opts}),
+  defp setup_commands(%{ch: channel} = opts),
     do: append_command(opts, :ch, {&MidiCleaner.set_midi_channel/2, [channel]})
 
   defp append_command(opts, key, command) do
     remaining_opts = Map.delete(opts, key)
-    [command | setup_commands({remaining_opts})]
+    [command | setup_commands(remaining_opts)]
   end
 
   defp run(commands) do
