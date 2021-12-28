58bacd2368ade8d7d0b4420b81305d88ad7821f1
Pat Maddox <pat@patmaddox.com>
2021-12-27 17:41:17 -0800

    FileList enumerates parent dirs

--- lib/midi_cleaner/file_list.ex
+++ lib/midi_cleaner/file_list.ex
@@ -9,6 +9,8 @@ defmodule MidiCleaner.FileList do
 
   def each_file(%{paths: paths}, func), do: Enum.each(paths, &process_path(&1, func))
 
+  def each_parent_dir(%{paths: paths}, func), do: set_of_parent_dirs(paths) |> Enum.each(func)
+
   defp file_or_dir_tuple(path) do
     if Path.extname(path) == ".mid" do
       {:file, path}
@@ -17,10 +19,31 @@ defmodule MidiCleaner.FileList do
     end
   end
 
-  def process_path({:file, path}, func), do: func.(path)
+  defp process_path({:file, path}, func), do: func.(path)
 
-  def process_path({:dir, path}, func) do
+  defp process_path({:dir, path}, func) do
     Path.wildcard("#{path}/**/*.mid")
     |> Enum.each(&process_path({:file, &1}, func))
   end
+
+  defp set_of_parent_dirs(paths), do: set_of_parent_dirs(MapSet.new(), paths)
+
+  defp set_of_parent_dirs(parent_dirs, []), do: parent_dirs
+
+  defp set_of_parent_dirs(parent_dirs, [{:file, path} | rest]) do
+    parent_dirs
+    |> add_parent_dir(path)
+    |> set_of_parent_dirs(rest)
+  end
+
+  defp set_of_parent_dirs(parent_dirs, [{:dir, path} | rest]) do
+    Path.wildcard("#{path}/**/*.mid")
+    |> Enum.reduce(parent_dirs, &add_parent_dir/2)
+    |> set_of_parent_dirs(rest)
+  end
+
+  defp add_parent_dir(%MapSet{} = parent_dirs, path),
+    do: MapSet.put(parent_dirs, Path.dirname(path))
+
+  defp add_parent_dir(path, %MapSet{} = parent_dirs), do: add_parent_dir(parent_dirs, path)
 end
--- test/file_list_test.exs
+++ test/file_list_test.exs
@@ -4,19 +4,47 @@ defmodule MidiCleaner.FileListTest do
 
   alias MidiCleaner.FileList
 
-  test "list of midi files" do
-    FileList.new(["1.mid", "2.mid"])
-    |> FileList.each_file(&send(self(), {:file, &1}))
+  describe "each_file()" do
+    test "list of midi files" do
+      FileList.new(["1.mid", "2.mid"])
+      |> FileList.each_file(&send(self(), {:file, &1}))
 
-    assert_received({:file, "1.mid"})
-    assert_received({:file, "2.mid"})
+      assert_received({:file, "1.mid"})
+      assert_received({:file, "2.mid"})
+    end
+
+    test "dir" do
+      FileList.new(["test/fixtures"])
+      |> FileList.each_file(&send(self(), {:file, &1}))
+
+      assert_received({:file, "test/fixtures/midi/drums.mid"})
+      assert_received({:file, "test/fixtures/midi/example.mid"})
+    end
   end
 
-  test "dir" do
-    FileList.new(["test/fixtures"])
-    |> FileList.each_file(&send(self(), {:file, &1}))
+  describe "each_parent_dir()" do
+    test "list of midi files" do
+      FileList.new(["midi/drums/1.mid", "midi/bass/2.mid"])
+      |> FileList.each_parent_dir(&send(self(), {:parent_dir, &1}))
 
-    assert_received({:file, "test/fixtures/midi/drums.mid"})
-    assert_received({:file, "test/fixtures/midi/example.mid"})
+      assert_received({:parent_dir, "midi/drums"})
+      assert_received({:parent_dir, "midi/bass"})
+    end
+
+    test "dir" do
+      FileList.new(["test/fixtures"])
+      |> FileList.each_parent_dir(&send(self(), {:parent_dir, &1}))
+
+      assert_received({:parent_dir, "test/fixtures/midi"})
+    end
+
+    test "files and dirs mixed" do
+      FileList.new(["midi/drums/1.mid", "test/fixtures", "midi/2.mid", "test/fixtures/midi"])
+      |> FileList.each_parent_dir(&send(self(), {:parent_dir, &1}))
+
+      assert_received({:parent_dir, "midi/drums"})
+      assert_received({:parent_dir, "midi"})
+      assert_received({:parent_dir, "test/fixtures/midi"})
+    end
   end
 end
